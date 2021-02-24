c991d9f276b5a48ba57baa41fb8dbd9250567983
Pradeep <Pradeep@LAPTOP-82NDSM1Q>
2021-02-24 17:53:16 +0530

    Open Browser

---
+++ NTGPOC/src/main/java/openBrowser/ChromeBrowser.java
@@ -0,0 +1,20 @@
+package openBrowser;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+
+public class ChromeBrowser {
+	
+	
+	public static void main(String[] args) {
+		WebDriver driver;
+		System.setProperty("webdriver.ie.driver", "E:\\NTG Jenkins\\JenkinsPOC\\driver\\IEDriverServer.exe");
+		driver = new InternetExplorerDriver();
+		driver.get("https://www.amazon.in");
+		
+	}
+	
+	
+
+}
